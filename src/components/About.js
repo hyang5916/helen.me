8d677b6f3d321f4accc43ae7b4be78a8ade31404
Helen Yang <helenyang@helens-air.lan>
2021-07-14 00:59:04 -0700

    added formatting

--- src/components/About.js
+++ src/components/About.js
@@ -7,16 +7,29 @@ import './About.css';
 import cat from '/Users/helenyang/Desktop/helens-web/src/cat-in-black-silhouette.svg';
 import heart from '/Users/helenyang/Desktop/helens-web/src/like.svg';
 import Header from './Header.js';
+import me from '../me.png';
+
+var phantom = {
+  display: 'block',
+  // padding: '10vh',
+  paddingTop: '10vh',
+  height: 'auto',
+  width: '100%',
+}
 
 function About() {
     return (
       <div className="App">
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <header className="App-header">
           <div className="Nav">
             <Header />
           </div>
           <div>
           <img src={helenyang} className="App-name" alt="logo" />
+          </div>
+          <div>
+          <img src={me} className="me" alt="logo" />
           </div>
           <div className="typewriter">
             <h1>developer + ml researcher + musician</h1>
@@ -25,11 +38,11 @@ function About() {
             <h1>Hello! I'm Helen, an undergraduate student at MIT double majoring in computer science and cognitive science. 
               In the past, I've worked on projects in deep learning, full-stack development, bayesian inference, and natural language processing. 
               Outside of my major, some of my favorite things include  violin performance (I've been 
-              playing for 15 years!), puzzles, fencing (the sport), photography, Pokemon, and hot pot.
+              playing for 15 years!), puzzles, fencing (the sport), photography, Pokemon, and hot pot :)
 
             </h1>
             <h1>
-            Check out my favorite projects and ~artistic~ works using the nav bar at the top :)
+            {/* Check out my favorite projects and ~artistic~ works using the nav bar at the top :) */}
             </h1>
           </div>
           <div>
@@ -37,9 +50,12 @@ function About() {
           </div>
         </header>
         <footer className="App-footer">
-          <h2>
-            made with <img src={heart} className="App-logo" alt="logo" /> and <img src={cat} className="App-logo" alt="logo" />
-          </h2>
+          {/* <div style={phantom}/> */}
+          {/* <div className="App-footer"> */}
+            <h2 >
+              made with <img src={heart} className="App-logo" alt="logo" /> and <img src={cat} className="App-logo" alt="logo" />
+            </h2>
+          {/* </div> */}
         </footer>
       </div>
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,24 +3,67 @@ import Header from "./Header";
 import helenyang from '/Users/helenyang/Desktop/helens-web/src/helenyang.png';
 import cat from '/Users/helenyang/Desktop/helens-web/src/cat-in-black-silhouette.svg';
 import heart from '/Users/helenyang/Desktop/helens-web/src/like.svg';
+import icecreme1 from '../project_pics/icecreme-brulee1.png';
+import icecreme2 from '../project_pics/icecreme-brulee1.png';
+import inception2 from '../project_pics/inception2.png';
+import xfair from '../project_pics/xfair.png';
+import ocean from '../project_pics/ocean.png';
+import neuron from '../project_pics/neuron.gif';
 import './Projects.css';
 
 function Projects() {
     return (
       <div className="Projects">
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <header className="App-header">
           <div className="Nav">
             <Header />
           </div>
           <div className="text">
-              <h2>Here are some projects that I've worked on in the past! </h2>
+              <h2>Here are some <span class="pinktext">projects</span> that I've worked on in the past! </h2>
           </div>
-          
+            <div className="icecreme">
+                <a href="https://icecreme-brulee.herokuapp.com/" target="_blank">
+                    <img src={icecreme1} className="creme1"/> 
+                </a>
+                {/* <div className="icecreme-title">
+                <h1>icecreme.brulee</h1> 
+            </div> */}
+                <div className="icecremetext">
+                    <h1 className="icecreme-title">icecreme.brulee</h1> 
+                    <h2>icecreme.brulee is a full-stack web application game that I created with two friends.
+                        Since our first semester of college was virtual, we wanted to make a fun way for new friends 
+                        to get to know each other better. This website is a creative variation of the popular party game Paranoia. 
+                        To use, a host can create a room, and friends can join using a uniquely-generated code. 
+                    </h2> 
+                    <h3>
+                        Note: need at least two people to play
+                    </h3>
+                    <h2>
+                        TypeScript, React, Node.js, MongoDB, Bootstrap
+                    </h2>
+                </div>
+            </div>
+            {/* <hr className="line"/>        */}
+            <div className="inception2">
+                {/* <img src={inception2} className="inception"/> */}
+                <img src={neuron} className="inception"/>
+            </div>
+            <div>
+                <a href="https://xfair.io/" target="_blank">
+                    <img src={xfair} className="xfair"/>
+                </a>
+            </div>
+
+            <div>
+                <a href="https://anoceanincrisis.wordpress.com/" target="_blank">
+                    <img src={ocean} className="ocean"/>
+                </a>
+            </div>
+            
         </header>
         <footer className="App-footer">
-          <p>
-            made with <img src={heart} className="App-logo" alt="logo" /> and <img src={cat} className="App-logo" alt="logo" />
-          </p>
+
         </footer>
     </div>
     )
